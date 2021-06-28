d0d4a4e3fb0e467852146a11f769349043af6198
Udoka Okoye <leviokoye@gmail.com>
2021-06-28 12:18:50 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Header from './Pages/Components/Header/Header.js'
+import Home from './Pages/Home/Home.js';
+import Footer from './Pages/Components/Footer/Footer.js'
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
